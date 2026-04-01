90da812d12848f0ba821b9b9789209035beeceec
Mircea Staicu <md.staicu@yahoo.com>
2026-04-01 11:22:16 +0100

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,13 @@ if ! command -v brew &>/dev/null; then
 fi
 
 # Load brew into current shell
-eval "$(brew shellenv)"
+if command -v brew &>/dev/null; then
+  eval "$(brew shellenv)"
+elif [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /usr/local/bin/brew ]]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
 
 echo "📦 Installing Brew bundle..."
 brew bundle --file="Brewfile"
